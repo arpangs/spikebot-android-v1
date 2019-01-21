eb99bd32f052252245c275b2d617b6fd0187ba63
pmehul55 <pmehul55@gmail.com>
2019-01-21 20:03:14 +0530

    spike

---
+++ app/src/main/java/com/spike/bot/listener/FilterMarkAll.java
@@ -0,0 +1,14 @@
+package com.spike.bot.listener;
+
+import com.spike.bot.model.Filter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sagar on 1/1/19.
+ * Gmail : jethvasagar2@gmail.com
+ */
+public interface FilterMarkAll {
+
+    public void filterAllMark(ArrayList<Filter> filters);
+}
